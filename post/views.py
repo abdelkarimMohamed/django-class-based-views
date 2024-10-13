274d3753bd707b452b50704be0d6c5a4e41a43c1
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-10-13 15:05:25 +0300

    add class based view structure

--- post/views.py
+++ post/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
+from .models import Post
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
+
+class PostDetail(DetailView):
+    model = Post
+
+class PostCreate(CreateView):
+    model = Post
+
+class PostDelete(DeleteView):
+    model = Post
+
+class PostUpdate(UpdateView):
+    model = Post
+
+
